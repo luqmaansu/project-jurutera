6950d1d0efe3e72d8293a23e050d41e17133e452
Luqmaan S <46421285+luqmaansu@users.noreply.github.com>
2022-04-26 17:24:05 +0800

    completed episode 8

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zbx996k=gv@zsjjs@ik)yislz&+uzp3y$(t0o50jh+ds5af-hv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                 'localhost',
